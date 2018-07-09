bd3450982c23fcfce13e5b751f6939b42f9a29c6
Герман <stgoge@ya.ru>
2018-07-09 10:54:44 +0700

    symbol)

--- solution.js
+++ solution.js
@@ -1,12 +1,12 @@
 const solution = (zeroes, ones) => {
-  const count = (lastSimbol, zeroCount, oneCount) => {
+  const count = (lastSymbol, zeroCount, oneCount) => {
     if (zeroCount === 0) {
       return oneCount > 0 ? count(1, zeroCount, oneCount - 1) : 1;
     }
     if (oneCount === 0) {
-      return lastSimbol === 1 ? count(0, zeroCount - 1, oneCount) : 0;
+      return lastSymbol === 1 ? count(0, zeroCount - 1, oneCount) : 0;
     }
-    if (lastSimbol === 0) return count(1, zeroCount, oneCount - 1);
+    if (lastSymbol === 0) return count(1, zeroCount, oneCount - 1);
     return count(1, zeroCount, oneCount - 1) + count(0, zeroCount - 1, oneCount);
   };
   return count(null, zeroes, ones);
